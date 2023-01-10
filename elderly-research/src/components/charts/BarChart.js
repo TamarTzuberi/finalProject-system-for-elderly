70947e624b3278a99f689163426ed7eec1839191
navitbranin <navitbranin123@gmail.com>
2023-01-10 16:46:18 +0200

    showing two charts on graph

--- elderly-research/src/components/charts/BarChart.js
+++ elderly-research/src/components/charts/BarChart.js
@@ -5,87 +5,154 @@ import { Bar, Line } from 'react-chartjs-2';
 
 import Chart from 'chart.js/auto';
 // import Sidebar from "../sidebar/Sidebar";
+import '/Users/navitbranin/finalProject/finalProject-system-for-elderly/finalProject-system-for-elderly/elderly-research/src/App.css'
 
 const BarChart = (props) => {
     console.log("props ", props)
-    const backgroundColors =[]
-    
+    const backgroundColors = []
+
     const getRandomColor = () => {
         var letters = '0123456789ABCDEF'.split('');
         var color = '#';
-        for (var i = 0; i < 6; i++ ) {
+        for (var i = 0; i < 6; i++) {
             color += letters[Math.floor(Math.random() * 16)];
         }
         return color;
-            }
-    const getBackgroundColors = () =>{
+    }
+    const getBackgroundColors = () => {
         // for (var i=0;i<props.labels.length; i++){
-            
+
         // }
         backgroundColors.push(getRandomColor());
         return backgroundColors;
     }
+    const colorObjective = getRandomColor();
+    const colorSubjective = getRandomColor();
+
     const labels = props.labels.map(a => a.toString())
     let state = {
         labels: labels,
         datasets: [
             {
-                label: props.label,
-                fill: true,
-                backgroundColor: getBackgroundColors(),
+                label: props.labelObjective,
+                fill: false,
+                backgroundColor: colorObjective,
                 borderColor: 'rgba(0,0,0,0.9)',
                 borderWidth: 2,
-                data: props.data
+                data: props.dataObjective,
+                yAxisID: 'A',
+                pointStyle: 'circle',
+                pointRadius: 2
+            },
+            {
+                label: props.labelSubjective,
+                fill: false,
+                backgroundColor: colorSubjective,
+                borderColor: 'rgba(0,0,0,0.9)',
+                borderWidth: 2,
+                data: props.dataSubjective,
+                yAxisID: 'B',
+                pointStyle: props.pointsStyle,
+                pointRadius: props.pointsRadius
+
             }
         ]
     }
     console.log("state ", state.labels)
     console.log("dataset ", state.datasets[0].data)
-    return(
-    <div className="charts">
-    <Bar style={{ textAlign: 'center' }}
-                        data={state}
-                        options={{
-                            title: {
-                                display: true,
-                                fontSize: 20
-                            },
-                            legend: {
-                                display: true,
-                                position: 'right'
-                            },
-                            scales: {
-                                y:
-                                {
-                                    min: props.min,
-                                    max: props.max,
-                                    stepSize: 1,
-                                },
+    return (
+        <div className="charts">
+            <Bar style={{ textAlign: 'center' }}
+                data={state}
+                options={{
+                    title: {
+                        display: true,
+                        fontSize: 20
+                    },
+                    legend: {
+                        display: true,
+                        // position: 'right'
+                    },
+                    scales: {
+                        A:
+                        {
+                            type: 'linear',
+                            min: props.minObjective,
+                            max: props.maxObjective,
+                            // ticks: {
+                            //     stepSize: 1
+                            //     },
+                            position: 'left',
+                        },
+                        B:
+                        {
+                            type: 'linear',
+                            min: props.minSubjective,
+                            max: props.maxSubjective,
+                            // ticks: {
+                            // stepSize: 1
+                            // },
+                            position: 'right',
+                        }
+
+                    }
+                }}
+            />
+            <Line style={{ textAlign: 'center' }}
+                data={state}
+                options={{
+                    plugins:{
+                    annotation: {
+                        annotations: [{
+                         point1: {
+                            type: 'point',
+                            xValue: 1,
+                            yValue: 5,
+                            borderColor: 'red',
+                            borderWidth: 4,
+                            backgroundColor: 'pink'
+                          }
+                        }
+                        ],
+                      },
+                    },
+                    title: {
+                        display: true,
+                        fontSize: 20
+                    },
+                    legend: {
+                        display: true,
+                        // position: 'right'
+                    },
+                    scales: {
+                        A:
+                        {
+                            type: 'linear',
+                            min: props.minObjective,
+                            max: props.maxObjective,
+                            // ticks: {
+
+                            //     stepSize: 1
+                            //     },
+                            position: 'left',
+                        },
+                        B:
+                        {
+                            type: 'linear',
+                            min: props.minSubjective,
+                            max: props.maxSubjective,
+                            // ticks: {
+                            // stepSize: 1
+                            // },
+                            position: 'right',
+                            grid: {
+                                drawOnChartArea: true
                             }
-                        }}
-                    />
-                    <Line style={{ textAlign: 'center' }}
-                        data={state}
-                        options={{
-                            title: {
-                                display: true,
-                                fontSize: 20
-                            },
-                            legend: {
-                                display: true,
-                                position: 'right'
-                            },
-                            scales: {
-                                y:
-                                {
-                                    min: props.min,
-                                    max: props.max,
-                                    stepSize: 1,
-                                },
-                            }
-                        }} />
-    </div>
-        );
+                        }
+                    }
+                }} />
+        </div>
+    );
 
 }
 export default BarChart;
--- elderly-research/src/components/pages/ResearcherPage.js
+++ elderly-research/src/components/pages/ResearcherPage.js
@@ -7,18 +7,24 @@ import axios from 'axios';
 // import Chart from 'chart.js/auto';
 // import { serverURL } from '../../ClientUtils'
 import BarChart from '../charts/BarChart';
+import './ResearcherPage.css';
 // import {hasCookie} from '../CookieManager';
 
 
 function ResearcherPage(props) {
-    const [showBar, setShowBar] = useState(false);
-    // const [showLine, setShowLine] = useState(false);
-    const [data, setData] = useState([]);
-    const [label, setLabel] = useState('');
+    const [showBarObjective, setShowBarObjective] = useState(false);
+    const [showBarSubjective, setShowBarSubjective] = useState(false);
+    const [dataObjective, setDataObjective] = useState([]);
+    const [dataSubjective, setDataSubjective] = useState([]);
+    const [labelObjective, setLabelObjective] = useState('');
+    const [labelSubjective, setLabelSubjective] = useState('');
     const [labels, setLabels] = useState([]);
-    const [min, setMin] = useState(0);
-    const [max, setMax] = useState(0);
-
+    const [minObjective, setMinObjective] = useState(0);
+    const [maxObjective, setMaxObjective] = useState(0);
+    const [minSubjective, setMinSubjective] = useState(0);
+    const [maxSubjective, setMaxSubjective] = useState(0);
+    const [pointsStyle, setPointsStyle] = useState([]);
+    const [pointsRadius, setPointsRadius] = useState([]);
     const handleStart = (s) => {
         if (s) {
             setStart(s)
@@ -35,54 +41,17 @@ function ResearcherPage(props) {
     const [end, setEnd] = useState();
 
 
-    // const setLabelsValue = (array,st) => {
-    //     let arr = [];
-    //     let s = (start) ? new Date(start) : new Date(st);
-    //     for (let index = 0; index < array.length; index++) {
-    //         s.setDate(s.getDate() + 1);
-    //         arr.push(`${s.getFullYear()}-${s.getMonth() + 1}-${s.getDate()}`);
-    //     }
-    //     setLabels(arr)
-    // }
-
-    // const getFeatures = async () => {
-    //     return await axios.get(`${serverURL}/researcher/features/${start}/${end}`);
-
-    // }
-
-    // const setForGragh = async (arr) => {
-    //     if (arr.length > 0) {
-    //         setData(arr[0]);
-    //         setLabelsValue(arr[0], +arr[1]);
-    //     } else {
-    //         setData(arr);
-    //         setLabelsValue(arr);
-    //     }
-
-    //     (showBar) ? setShowBar(false) : setShowBar(true);
-    // }
-
-
     const result1 = {value: 4000, start: 1671919200000}
     const result2 = {value: 3500, start: 1672005600000}
     const result3 = {value: 8000, start: 1672092000000}
     const arr1 = [result1, result2, result3]
 
     const showSteps = async (e) => {
-        // let response = [10, 20, 45, 30, 55, 70, 60]//await getFeatures();
-        extract(arr1)
-        // let response = await getFeatures();
-        // let arr = [];
-        // if (response.data.Steps.length > 0) {
-        //     arr = extract(response.data.Steps[0].days, response.data.Steps[0].start);
-        //     arr = extract(response.data.Steps[0].days, response.data.Steps[0].start);
-
-
-        // }
-        setLabel('Steps');
-        setMin(0);
-        setMax(10000);
-        setShowBar(true);
+        extract(arr1,"obj")
+        setLabelObjective('Steps');
+        setMinObjective(0);
+        setMaxObjective(10000);
+        setShowBarObjective(true);
     }
 
     const result7 = {value: 85, start: 1671919200000}
@@ -91,20 +60,13 @@ function ResearcherPage(props) {
     const arr3 = [result7, result8, result9]
 
     const showHR = async () => {
-        extract(arr3)
-        // let response = await getFeatures();
-        // let arr = [];
-        // // console.log(response);
-        // if (response.data.HR.length > 0) {
-        //     arr = extract(response.data.HR[0].days, response.data.HR[0].start);
 
+        extract(arr3,"obj")
+        setLabelObjective('HR');
+        setMinObjective(0);
+        setMaxObjective(130);
+        setShowBarObjective(true);
 
-        // }
-        setLabel('HR');
-        setMin(0);
-        setMax(130);
-        // setForGragh(arr);
-        setShowBar(true);
     }
 
     const result10 = {value: 8000, start: 1671919200000}
@@ -113,18 +75,12 @@ function ResearcherPage(props) {
     const arr4 = [result10, result11, result12]
 
     const showDistance = async () => {
-        extract(arr4)
-        // let response = await getFeatures();
-        // let arr = [];
-        // if (response.data.Distance.length > 0) {
-        //     arr = extract(response.data.Distance[0].days, response.data.Distance[0].start);
+        extract(arr4,"obj")
+        setLabelObjective('Distance');
+        setMinObjective(0);
+        setMaxObjective(15000);
+        setShowBarObjective(true);
 
-        // }
-        setLabel('Distance');
-        setMin(0);
-        setMax(15000);
-        // setForGragh(arr);
-        setShowBar(true);
     }
 
     const result13 = {value: 50, start: 1671919200000}
@@ -133,18 +89,12 @@ function ResearcherPage(props) {
     const arr5 = [result13, result14, result15]
 
     const showAM = async () => {
-        extract(arr5)
-        // let response = await getFeatures();
-        // let arr = [];
-        // if (response.data.Active_min.length > 0) {
-        //     arr = extract(response.data.Active_min[0].days, response.data.Active_min[0].start);
+        extract(arr5,"obj" )
+        setLabelObjective('Active Minutes');
+        setMinObjective(0);
+        setMaxObjective(300);
+        setShowBarObjective(true);
 
-        // }
-        setLabel('Active Minutes');
-        setMin(0);
-        setMax(300);
-        // setForGragh(arr);
-        setShowBar(true);
     }
 
 
@@ -154,22 +104,35 @@ function ResearcherPage(props) {
     const arr2 = [result4, result5, result6]
 
     const showSleep = async () => {
-        extract(arr2)
-        // let arr ={'days' :[
-        //     { 'day1': 3 }, { 'day2': 5 }, { 'day3': 4 }, { 'day4': 6 }, { 'day5': 8 }, { 'day6': 7 }, { 'day7': 4 }, { 'day8': 4 }, { 'day9': 7 }, { 'day10': 5 }, { 'day11': 3 }, { 'day12': 4 }, { 'day13': 5 }, { 'day14': 8 }, { 'day15': 7 }, { 'day16': 3 }, { 'day17': 5 }, { 'day18': 9 }, { 'day19': 4 }, { 'day20': 5 }, { 'day21': 7 }, { 'day22': 6 }, { 'day23': 5 }, { 'day24': 7 }, { 'day25': 4 }, { 'day26': 3 }, { 'day27': 6 }, { 'day28': 8 }, { 'day29': 10 }, { 'day30': 6 }
-        // ]}
-        // let response = await getFeatures();
-        // let arr = [];
-        // if (response.data.Sleeping.length > 0) {
-        // arr = extract(arr, 1647076066984);
+        extract(arr2, "obj")
+        setLabelObjective('Hours of Sleep');
+        setMinObjective(0);
+        setMaxObjective(10);
+        setShowBarObjective(true);
+    }
 
-        // }
-        setLabel('Hour of Sleeps');
-        setMin(0);
-        setMax(10);
-        // setForGragh(arr);
-        setShowBar(true);
+    const showHappiness = async () => {
+        extract(arr2,"sub")
+        setLabelSubjective('Happines Rate');
+        setMinSubjective(0);
+        setMaxSubjective(10);
+        setShowBarSubjective(true);
 
+    }
+    const showLoneliness = async () => {
+        extract(arr2, "sub")
+        setLabelSubjective('Lonliness Rate');
+        setMinSubjective(0);
+        setMaxSubjective(20);
+        setShowBarSubjective(true);
+
+    }
+    const showSelfHealth = async () => {
+        extract(arr2, "sub")
+        setLabelSubjective('Self Health Rate');
+        setMinSubjective(0);
+        setMaxSubjective(10);
+        setShowBarSubjective(true);
     }
 
     const stringToDate = (date) =>{
@@ -178,93 +141,60 @@ function ResearcherPage(props) {
         const month = today.getMonth();
         const day = today.getDate();
         const dateString = `${day}/${month + 1}/${year}`;
-
         return dateString
-    } 
+    }
 
-    const extract = (arr) => {
+    const meetingDatesArr =[1671919200000,1672005600000]; //getFunction
+
+    const extract = async (dataDateArr, dataType) => {
         let dataArr = []
         let dateArr = []
-        for (let key in arr) {
-            dataArr.push(arr[key].value)
-            dateArr.push(stringToDate(arr[key].start))
+        let pointsStyleArr = []
+        let pointsRadiusArr = []
+        for (let key in dataDateArr) {
+            let dataVal = dataDateArr[key].value
+            let date = dataDateArr[key].start
+            dataArr.push(dataVal)
+            dateArr.push(stringToDate(date))
+            if (meetingDatesArr.includes(date)) {
+                pointsStyleArr.push('star')
+                pointsRadiusArr.push(10)
+            }
+            else {
+                pointsStyleArr.push('circle')
+                pointsRadiusArr.push(2)
+            }
         }
-        setData(dataArr)
+        if (dataType ==='obj') {
+            setDataObjective(dataArr)
+        }
+        if (dataType === 'sub'){
+            setDataSubjective(dataArr)
+        }
         setLabels(dateArr)
+        setPointsStyle(pointsStyleArr)
+        setPointsRadius(pointsRadiusArr)
     }
-    // const extract = (days_dict, startPulled) => {
-    //     let result = []
-    //     if (!start || !end || start < startPulled) {
-    //         for (let key in days_dict) {
-    //             result.push(Object.values(days_dict[key])[0])
-    //         }
-    //     }
-    //     else {
-    //         let arr = [];
-    //         let s = new Date(start);
-    //         let e = new Date(end);
-    //         const numOfDaysGraph = Math.ceil(Math.abs(e - s) / (1000 * 60 * 60 * 24));
-    //         if (parseInt(startPulled) === s.getTime()) {
-    //             arr = days_dict.slice(0, numOfDaysGraph);
-    //         }
-    //         let startP = new Date(+startPulled);
-    //         const DiffStarts = Math.ceil(Math.abs(s - startP) / (1000 * 60 * 60 * 24));
-    //         arr = (numOfDaysGraph + DiffStarts > days_dict.length) ? days_dict.slice(DiffStarts, days_dict.length) : days_dict.slice(DiffStarts, numOfDaysGraph + DiffStarts);
-    //         console.log(arr);
-    //         for (let key in arr) {
-    //             result.push(Object.values(arr[key])[0])
-    //         }
-    //     }
-
-    //     return [result, startPulled];
-    // }
-
-    // const downloadToCsv = async () => {
-    //     // setStart(null);
-    //     // setEnd(null);
-    //     let features = await axios.get(`${serverURL}/researcher/features/${null}/${null}`);
-    //     features = features.data;
-    //     console.log(features);
-    //     let rows = [];
-    //     rows.push(['user', 'Steps', 'Calories', 'Speed', 'Distance', 'Active_min'])
-    //     for (let day = 0; day < features['Steps'][0].days.length; day++) {
-    //         let step = (day < features['Steps'][0].days.length) ? Object.values(features['Steps'][0].days[day])[0] : 0;
-    //         let calories = (day < features['Calories'][0].days.length) ? Object.values(features['Calories'][0].days[day])[0] : 0;
-    //         let speed = (day < features['Speed'][0].days.length) ? Object.values(features['Speed'][0].days[day])[0] : 0;
-    //         let distance = (day < features['Distance'][0].days.length) ? Object.values(features['Distance'][0].days[day])[0] : 0;
-    //         let am = (day < features['Active_min'][0].days.length) ? Object.values(features['Active_min'][0].days[day])[0] : 0;
-    //         let uid = features['Active_min'][0].googleid;
-    //         rows.push([uid, step + '', calories + '', speed + '', distance + '', am + '']);
-    //     }
-
-
-    //     let csvContent = "data:text/csv;charset=utf-8,"
-    //         + rows.map(e => e.join(",")).join("\n");
-
-
-    //     let encodedUri = encodeURI(csvContent);
-    //     window.open(encodedUri);
-
-    //     // console.log(hasCookie());
-    //     let forms = await axios.get(`${serverURL}/researcher/allDailyForms`);
-    //     forms =forms.data;
-    //     let rowsDaily = [];
-    //     rowsDaily.push(['user', 'happy', 'lonely', 'pain', 'sad', 'sleep', 'Date']);
-    //     for (let i = 0; i < forms.length; i++) {
-    //         rowsDaily.push([forms[i].googleid,forms[i].answers.happy +'',forms[i].answers.lonely +'',forms[i].answers.pain+'',forms[i].answers.sad+'',forms[i].answers.sleep+'', forms[i].Date]);
-    //     }
-
-    //     let csvContentDaily = "data:text/csv;charset=utf-8,"
-    //         + rowsDaily.map(e => e.join(",")).join("\n");
-
-
-    //     let encodedUriDaily = encodeURI(csvContentDaily);
-    //     window.open(encodedUriDaily);
-
-
-    // }
-
-    const content = (
+    const contentSobjectiveData = (
+        <div className="buttons-section">
+        <button
+            className="sb-btn"
+            onClick={() => showHappiness()}>
+            Happiness
+        </button>
+        <button
+            className="sb-btn"
+            onClick={() => showLoneliness()}>
+            Loneliness
+        </button>
+        <button
+            className="sb-btn"
+            onClick={() => showSelfHealth()}>
+            Self Health
+        </button>
+        </div>
+    )
+    const contentObjectiveData = (
         <div className="buttons-section">
             <button
                 className="sb-btn"
@@ -289,7 +219,7 @@ function ResearcherPage(props) {
             <button
                 className="sb-btn"
                 onClick={() => showSleep()}>
-                Hour of Sleeps
+                Hours of Sleep
             </button>
             {/* <button
                 className="sb-btn"
@@ -297,25 +227,27 @@ function ResearcherPage(props) {
                 הורדת קובץ
             </button> */}
 
-
         </div>
     );
 
     return (
         <div className="page">
-            <Sidebar history={props.history} content={content} />
-
-            <div style={{ position: 'absolute', top: 20, left: '65%', width: '100px', backgroundColor: 'white' }}>
-                <input type='date' className='start' value={start} onChange={e => handleStart(e.target.value)} />
-                <input type='date' className='end' value={end} onChange={e => handleEnd(e.target.value)} />
+            <div className="leftContainer">
+            <Sidebar history={props.history} content={contentObjectiveData} />
             </div>
-            {showBar &&
-                <div style={{ position: "absolute", top: '100px', left: '25%', height: '50%', width: '40%', backgroundColor: 'white' }}>
-
-                    <BarChart data={data} label={label} labels={labels} min={min} max={max} />
+            <div className="middleContainer" style={{}}>
+                <div style={{ top: 20, left: '65%', width: '100px', backgroundColor: 'white', marginRight: '150px'}}>
+                    <input type='date' className='start' value={start} onChange={e => handleStart(e.target.value)} />
+                    <input type='date' className='end' value={end} onChange={e => handleEnd(e.target.value)} />
                 </div>
-            }
-
+                    <div style={{ position: "absolute", top: '100px', left: '25%', height: '50%', width: '40%',backgroundColor: 'white', marginLeft: '70px'}}>
+                        {(showBarObjective || showBarSubjective) && <BarChart dataObjective={dataObjective} dataSubjective={dataSubjective} labelObjective={labelObjective} labelSubjective={labelSubjective} labels={labels} minObjective={minObjective} maxObjective={maxObjective} minSubjective={minSubjective} maxSubjective={maxSubjective} pointsStyle={pointsStyle} pointsRadius={pointsRadius}/>}
+                    </div>
+            
+            </div>  
+             <div className="rightContainer">
+            <Sidebar history={props.history} content={contentSobjectiveData} />
+            </div>
         </div>
     );
 }
