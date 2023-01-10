989641491c3789ead6de2fd2f818642077e3a8d9
navitbranin <navitbranin123@gmail.com>
2023-01-10 16:56:42 +0200

    fixed barChart

--- elderly-research/src/components/charts/BarChart.js
+++ elderly-research/src/components/charts/BarChart.js
@@ -5,7 +5,7 @@ import { Bar, Line } from 'react-chartjs-2';
 
 import Chart from 'chart.js/auto';
 // import Sidebar from "../sidebar/Sidebar";
-import '/src/App.css'
+// import 'elderly-research/src/App.css'
 
 const BarChart = (props) => {
     console.log("props ", props)
